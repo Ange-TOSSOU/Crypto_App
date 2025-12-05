d41ba08df1549efac80a938c23ec4372fa6d255b
louali <loualihiba2003@gmail.com>
2025-12-05 11:24:23 +0100

    Add authentication methods

--- src/app/shared/services/auth/auth.service.ts
+++ src/app/shared/services/auth/auth.service.ts
@@ -1,9 +1,56 @@
 import { Injectable } from '@angular/core';
-
+import {
+  Auth,
+  browserSessionPersistence,
+  createUserWithEmailAndPassword,
+  GoogleAuthProvider,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+  user,
+  User,
+} from '@angular/fire/auth';
+import { setPersistence } from 'firebase/auth';
+import { from, Observable } from 'rxjs';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
-
-  constructor() { }
+  user$: Observable<User | null>;
+  constructor(private firebaseAuth: Auth) {
+    this.setSessionStoragePersistence();
+    this.user$ = user(this.firebaseAuth);
+  }
+  private setSessionStoragePersistence(): void {
+    setPersistence(this.firebaseAuth, browserSessionPersistence);
+  }
+  signUp(email: string, password: string) {
+    const promise = createUserWithEmailAndPassword(
+      this.firebaseAuth,
+      email,
+      password
+    ).then(() => {
+      //
+    });
+    return from(promise);
+  }
+  login(email: string, password: string): Observable<void> {
+    const promise = signInWithEmailAndPassword(
+      this.firebaseAuth,
+      email,
+      password
+    ).then(() => {
+      //
+    });
+    return from(promise);
+  }
+  logout(): Observable<void> {
+    const promise = signOut(this.firebaseAuth).then(() => {
+      sessionStorage.clear();
+    });
+    return from(promise);
+  }
+  isAuthenticated(): boolean {
+    return !!localStorage.getItem('token');
+  }
 }
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: false,
+    firebaseConfig: {
+        projectId: "crypto-c0b3a",
+        appId: "1:870983626041:web:649fc48e6b81641ef40ce8",
+        storageBucket: "crypto-c0b3a.firebasestorage.app",
+        apiKey: "AIzaSyA2L6IyqFSmlADZrjTm-YczTmLs3wVCy0c",
+        authDomain: "crypto-c0b3a.firebaseapp.com",
+        messagingSenderId: "870983626041",
+    }
+};
