7d91ed881a3b088ff4e3b56842bf917ae3fd6885
Ange TOSSOU <angetossou.dev@gmail.com>
2026-01-08 10:32:56 +0100

    Add login page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,10 @@ import { Routes } from '@angular/router';
 import { redirectUnauthorizedTo, canActivate } from '@angular/fire/auth-guard';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { SigninComponent } from './pages/signin/signin.component';
-import { SignupComponent } from './pages/signup/signup.component';
 import { LoginComponent } from './pages/login/login.component';
 import { AppComponent } from './app.component';
 
-const redirectUnauthorizedToLogin = () => redirectUnauthorizedTo(['signin']);
+const redirectUnauthorizedToLogin = () => redirectUnauthorizedTo(['login']);
 
 export const routes: Routes = [
     {
