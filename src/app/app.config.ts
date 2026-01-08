47a462767823a5011d2fce4384e1364b667317bd
Ange TOSSOU <angetossou.dev@gmail.com>
2026-01-08 12:18:12 +0100

    Check redirection links and configuration files

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -19,6 +19,6 @@ export const appConfig: ApplicationConfig = {
     provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
     provideAuth(() => getAuth()),
     provideFirestore(() => getFirestore()),
-    { provide: LOCALE_ID, useValue: 'fr-FR' }
+    { provide: LOCALE_ID, useValue: 'fr-FR' }, provideFirebaseApp(() => initializeApp({ projectId: "crypto-c0b3a", appId: "1:870983626041:web:649fc48e6b81641ef40ce8", storageBucket: "crypto-c0b3a.firebasestorage.app", apiKey: "AIzaSyA2L6IyqFSmlADZrjTm-YczTmLs3wVCy0c", authDomain: "crypto-c0b3a.firebaseapp.com", messagingSenderId: "870983626041"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())
   ],
 };
